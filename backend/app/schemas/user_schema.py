cb4d20db3bb274e4b5c7558c83ea7beab00a5bd2
Milton Ytusaca <milton.yv98@gmail.com>
2026-05-23 16:51:40 -0500

    change backend schema user

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -1,25 +1,40 @@
 from pydantic import BaseModel, EmailStr, Field
 from typing import Optional, Dict, Any
 
+
 class UserCreate(BaseModel):
-  username: str = Field(..., min_length=3, max_length=50, description="Nombre de usuario")
-  email: EmailStr = Field(..., description="Correo electrónico del usuario")
-  password: str = Field(..., min_length=6, max_length=100, description="Contraseña del usuario")
+    username: str = Field(
+        ..., min_length=4, max_length=50, description="Nombre de usuario"
+    )
+    email: EmailStr = Field(..., description="Correo electrónico del usuario")
+    password: str = Field(
+        ..., min_length=4, max_length=100, description="Contraseña del usuario"
+    )
+
 
 class RegisterResponse(BaseModel):
     message: str
     user_id: int
 
+
 class UserLogin(BaseModel):
-  email: EmailStr = Field(..., description="Correo electrónico del usuario")
-  password: str = Field(..., min_length=6, max_length=100, description="Contraseña del usuario")
+    email: EmailStr = Field(..., description="Correo electrónico del usuario")
+    password: str = Field(
+        ..., min_length=4, max_length=100, description="Contraseña del usuario"
+    )
+
 
 class UserUpdate(BaseModel):
-  username: Optional[str] = Field(..., min_length=3, max_length=50, description="Nuevo nombre de usuario")
-  email: Optional[EmailStr] = Field(..., description="Nuevo correo electronico")
-  password: Optional[str] = Field(..., min_length=6, max_length=100, description="Contraseña antigua")
-  password_new: Optional[str]
+    username: Optional[str] = Field(
+        ..., min_length=4, max_length=50, description="Nuevo nombre de usuario"
+    )
+    email: Optional[EmailStr] = Field(..., description="Nuevo correo electronico")
+    password: Optional[str] = Field(
+        ..., min_length=4, max_length=100, description="Contraseña antigua"
+    )
+    password_new: Optional[str]
+
 
 class UserUpdateResponse(BaseModel):
-  message: str
-  data_new: Dict[str, Any] = Field(default_factory=dict)
+    message: str
+    data_new: Dict[str, Any] = Field(default_factory=dict)
